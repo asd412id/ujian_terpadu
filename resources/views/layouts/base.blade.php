2757b86bb51029fcb4359d58d34c7edafd81dab5
asd412id <asdar.binsyam@gmail.com>
2026-03-11 07:54:05 +0800

    fix: confirm modal SVG icon not rendering - use x-show instead of x-if inside SVG

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -56,13 +56,11 @@
                     <div class="px-6 py-5 text-center">
                         <div class="mx-auto w-12 h-12 rounded-full flex items-center justify-center mb-4"
                              :class="$store.confirmModal.danger ? 'bg-red-100' : 'bg-blue-100'">
-                            <svg class="w-6 h-6" :class="$store.confirmModal.danger ? 'text-red-600' : 'text-blue-600'" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <template x-if="$store.confirmModal.danger">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.27 16.5c-.77.833.192 2.5 1.732 2.5z"/>
-                                </template>
-                                <template x-if="!$store.confirmModal.danger">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                                </template>
+                            <svg x-show="$store.confirmModal.danger" class="w-6 h-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.27 16.5c-.77.833.192 2.5 1.732 2.5z"/>
+                            </svg>
+                            <svg x-show="!$store.confirmModal.danger" class="w-6 h-6 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                             </svg>
                         </div>
                         <h3 class="text-base font-semibold text-gray-900 mb-2" x-text="$store.confirmModal.title"></h3>
